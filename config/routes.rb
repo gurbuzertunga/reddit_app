0e76b6df05a359d7b97881f56667b3eacdb29cce
gurbuzertunga <gurbuzertunga@gmail.com>
2020-09-17 16:04:40 +0300

    creating a user interface

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'users/index'
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
-  root 'welcome#index'
+  resources :posts
+  resources :comments
+  root 'posts#index'
 end
