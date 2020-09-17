a39d8c8506d5dda32ac9abc5ca9205a53785fbaf
gurbuzertunga <gurbuzertunga@gmail.com>
2020-09-17 16:52:41 +0300

    added delete edit actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,50 @@
 class PostsController < ApplicationController
+  include PostsHelper
+
   def index
     @posts = Post.all
   end
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+    @post.user_id = params[:post][:user_id]
+    @post.save
+
+    flash.notice = "Post '#{@post.title}' Created!"
+
+    redirect_to posts_path(@post)
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    flash.notice = "Post '#{@post.title}' Deleted!"
+
+    redirect_to posts_path(@post)
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+
+    flash.notice = "Post '#{@post.title}' Updated!"
+
+
+    redirect_to posts_path(@post)
   end
 end
